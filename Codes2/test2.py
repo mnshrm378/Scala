5dfb614e1156cf5059cd6a51afa3c7772743cd3b
Ajay Singhal <ajay.singhal.external@veoneer.com>
2018-11-16 15:24:46 +0100

    added new content

--- Codes2/test2.py
+++ Codes2/test2.py
@@ -1,24 +1,25 @@
 import sys
 import json
-from pprint import pprint
-from elasticsearch import Elasticsearch
-es = Elasticsearch([{'host': 'localhost','port': '9200'}])
-MyFile= open("C:\\Users\\alay.singhal\\Downloads\\File conversion project\\After Markings1\\D0EvisionMetaData.json","r").read()
-#for line in MyFile:
-#    print(line)
-#MyFile.close()
+#from pprint import pprint
+#from elasticsearch import Elasticsearch
+#es = Elasticsearch([{'host': 'localhost','port': '9200'}])
 
-ClearData = MyFile.splitlines(True)
-i=0
-json_str=""
-docs ={}
-for line in ClearData:
-    line = ''.join(line.split())
-    if line != "},":
-        json_str = json_str+line
-    else:
-        docs[i]=json_str+"}"
-        json_str=""
-        print(docs[i])
-        es.index(index='fileinfo-markeddata1111', doc_type='Blog', id=i, body=docs[i])
-        i=i+1 
+MyFile1= open("C:\\Users\\alay.singhal\\Downloads\\File conversion project\\SMPC522A1DC0032_DE-PIAE1004_20170317_141659_FileInfo\\visionMetaData.json","r")
+x = MyFile1.readlines()
+print(x)
+MyFile1.close()
+
+#ClearData = MyFile.splitlines(True)
+#i=0
+#json_str=""
+#docs ={}
+#for line in ClearData:
+#    line = ''.join(line.split())
+#    if line != "},":
+#        json_str = json_str+line
+#    else:
+#        docs[i]=json_str+"}"
+#        json_str=""
+#        print(docs[i])
+#        es.index(index='fileinfo-markeddata1111', doc_type='Blog', id=i, body=docs[i])
+#        i=i+1 
