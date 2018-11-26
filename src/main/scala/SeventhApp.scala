8dfed52a9196c749b02f16f2741ea49682d5a99e
Ajay Singhal <ajay.singhal.external@veoneer.com>
2018-11-26 17:35:05 +0100

    added

--- src/main/scala/SeventhApp.scala
+++ src/main/scala/SeventhApp.scala
@@ -23,22 +23,24 @@ val newDS = sample1.zipWithIndex().map{case(line,i)=>i.toString + "," + line}
 
 val n = newDS.filter(line => line.contains("countryCode")).take(1).flatMap(line => line.split(","))
 val f = sc.parallelize(n).first().toInt
-newDS.take(f).foreach(println)
 
+newDS.take(f).slice(3,12).foreach(println)
+sample1.take(f).slice(3,f).foreach(println)
 val pairD = newDS.take(f).map(line => (line.split(","))).map(fields => (fields(1),fields(2)))
-pairD.foreach(println)
 
-print("to convert to Json this file contains data for: ")
-pairD.map(x => x._1).mkString(",").foreach(print)
+println("to convert to Json this file contains data for: ")
+print("[")
+val test = pairD.map(x => x._1).mkString(" , ").foreach(print)
+print("]\n")
 
-
+val n1 = Source.fromFile("C:\\Users\\alay.singhal\\Desktop\\Codes\\After_markings_SMPC522A1DC0040_SE-PGA454_20171012_082108_FileInfo.txt").getLines.slice(0,12)
+n1.slice(3,12).foreach(println)
 //print("to convert to Json")
 
 //.map(fields => (fields(0),fields(1)))
 //val getJsonFor = newDS.filter(line => line.startsWith("3,")).map(line => line.split(",")).map(fields => (fields(2),fields(3)))
 //val k = getJsonFor.keys.take(1).foreach(println)
 //val v = getJsonFor.values.take(1).foreach(println)
-
 
 sc.stop()
 }
